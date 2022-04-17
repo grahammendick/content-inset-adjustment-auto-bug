e091e285eb905d2dc1d433521312fc3e177a3f1b
Graham Mendick <graham.mendick@gmail.com>
2022-04-17 17:50:28 +0100

    Added hello world showing bug
    World screen has scrollview with contentInsetAdjustmentBehavior set to automatic. Steps to recreate
    1. Navigate next
    2. Navigate back
    3. Navigate next
    Then the scrollview will start at the top covered by the navigation bar instead of starting below it

---
+++ newarch/World.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ScrollView, View, StyleSheet} from 'react-native';
+import {NavigationBar} from 'navigation-react-native';
+
+const World = () => (
+  <>
+    <NavigationBar title="Hello" />
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <View style={styles.view} />
+    </ScrollView>
+  </>
+);
+
+const styles = StyleSheet.create({
+  view: {
+    height: 100,
+    backgroundColor: 'orange',
+  },
+});
+
+export default World;
